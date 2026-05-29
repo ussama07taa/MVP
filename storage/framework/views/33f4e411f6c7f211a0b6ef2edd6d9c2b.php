b2a4ba4c9d9e8a34cc2f956cca16437cc5938b97
ussama07taa <uss.taao@gmail.com>
2026-05-29 17:58:39 +0100

    Clean up storage files and fix .gitignore

--- storage/framework/views/33f4e411f6c7f211a0b6ef2edd6d9c2b.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\Users\Taaouati\Downloads\MVP\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
--- storage/framework/views/4afe68e42e0305816804fd870541ba0a.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH C:\Users\Taaouati\Downloads\MVP\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
